e4f835408eb9c2b139124e6d70391b722db4e630
darshankerkar <darshankerkar09@gmail.com>
2025-09-07 20:01:00 +0530

    Switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/AuthValidator">
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
+
