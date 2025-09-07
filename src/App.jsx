7ea6070469075cdad7e46d6050ed1e076f5e2cf8
darshankerkar <darshankerkar09@gmail.com>
2025-09-07 22:50:20 +0530

    AuthValidator added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path='/' element={<Welcome />} />
+          <Route path='/AuthValidator' element={<Welcome />} />
         </Routes>
     </>
   );
