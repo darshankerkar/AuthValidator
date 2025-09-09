61ec7da2fbe0b3c1e710b13d040bb90080e584f6
AtharvaC <atharvachavan527@gmail.com>
2025-09-09 22:45:28 +0530

    nmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -7,15 +7,13 @@ import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <>
-        <Routes>
-          <Route path='/' element={<Welcome />} />
-          <Route path='/login' element={<Login />} />
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/admin-dashboard" element={<Dashboard />} />
-        </Routes>
-    </>
+    <Routes>
+      <Route path='/' element={<Welcome />} />
+      <Route path='/login' element={<Login />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/admin-dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/AuthValidator">
     <App />
   </BrowserRouter>
 );
